8ab7b9dd32ebf1ba1464fdc6b54a1f7922c13ff6
egor <egazam@mail.ru>
2026-03-30 17:11:51 +0700

    add code

--- main.c
+++ main.c
@@ -1,7 +1,104 @@
 #include <stdio.h>
 
+void squeeze(char *s1, char *s2){
+    int i=0, k=0;
+    while(s1[i]){
+        int j=0;
+        int found=0;
+        while(s2[j] && !found){
+            if(s1[i]==s2[j]){
+                found=1;
+            }
+            j++;
+        }
+        if(!found){
+            s1[k++]=s1[i];
+        }
+        i++;
+    }
+    s1[k]='\0';
+    printf("%s\n",s1);
+}
+
+int charToValue(char text){
+    if(text>='0'&&text<='9'){
+        return text-'0';
+    }
+    return text-'A'+10;
+}
+
+void isDecMult4(char *text, int base){
+    int len=0;
+    while(text[len]!='\0'){
+        len++;
+    }
+    int value;
+    if(len==1){
+        value=charToValue(text[0]);
+    }
+    else{
+        int a=charToValue(text[len-2]);
+        int b=charToValue(text[len-1]);
+        value=a*base+b;
+    }
+    if(value%4==0){
+        printf("yes\n");
+    }
+    else{
+        printf("no\n");
+    }
+}
+
+void printDifWords(char *sq, char *W){
+    int i=0;
+    int wlen=0;
+    char tmp[100];
+    while(W[wlen]){
+        wlen++;
+    }
+    while(sq[i]){
+        while(sq[i]&&(sq[i]==','||sq[i]==' '||sq[i]=='.')){
+            i++;
+        }
+        if(!sq[i]){
+            break;
+        }
+        int j=0;
+        while(sq[i]&&sq[i]!=','&&sq[i]!=' '&&sq[i]!='.'){
+            tmp[j++]=sq[i++];
+        }
+        tmp[j]='\0';
+        int sqlen=j;
+        int eq=(sqlen==wlen);
+        if(eq){
+            int k=0;
+            while(k<sqlen){
+                if(tmp[k]!=W[k]){
+                    eq=0;
+                    break;
+                }
+                k++;
+            }
+        }
+        if(!eq){
+            printf("%s ",tmp);
+        }
+    }
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    char s1[]="asndfghasjklar";
+    char s2[]="sdafe";
+    squeeze(s1,s2);
+
+    char text[]="1C";
+    int base=16;
+    isDecMult4(text,base);
+
+    char sq[]="aaa, bbb, bbbc, bbbb, bb.";
+    char W[]="bbb";
+    printDifWords(sq,W);
+
     return 0;
 }
